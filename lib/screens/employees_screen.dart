df518ffc4680bd570876a292be0dfb844b16a84d
Kashinath Patekar <kashinathpatekar26@gmail.com>
2021-01-14 23:36:35 +0530

    Complete All_Employees page

--- lib/screens/employees_screen.dart
+++ lib/screens/employees_screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:employee_db/models/employee.dart';
+import 'package:employee_db/providers/organizations.dart';
 
 class EmployeesScreen extends StatelessWidget {
   static const routeName = '/add-emp';
 
   @override
   Widget build(BuildContext context) {
+    final organisations = Provider.of<Organizations>(context).list;
+    final List<Employee> employees =
+        organisations.fold([], (allEmps, org) => allEmps + org.employees);
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text('All Employees'),
+      appBar: AppBar(title: Text("All Employees")),
+      body: ListView.builder(
+        itemBuilder: (_, index) => ListTile(
+          title: Text("${employees[index].fullName}"),
+          subtitle: Text("EmpID: ${employees[index].employeeId}"),
+          trailing: Text("Age: ${employees[index].age}"),
         ),
-        body: Center(
-          child: Text('This is add employee page'),
-        ));
+        itemCount: employees.length,
+      ),
+    );
   }
 }
